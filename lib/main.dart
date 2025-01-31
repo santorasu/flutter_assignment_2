5b8a72e3dc1d7aaf33b5de32f240ca991954f5f3
Santo Rasu <santorasu09.com>
2025-02-01 00:58:48 +0600

    This is update of alartDialog for delete contact or cancel

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Contact List',
       theme: ThemeData(
-          primaryColor: Colors.teal,
-          appBarTheme: const AppBarTheme(
-              backgroundColor: Colors.teal,
-              foregroundColor: Colors.white,
-              centerTitle: true),
-          scaffoldBackgroundColor: Colors.grey.shade300),
-      home: MyHomePage(),
+        primaryColor: Colors.teal,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.teal,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+        ),
+        scaffoldBackgroundColor: Colors.grey.shade300,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
@@ -51,14 +53,35 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-   _deleteContact(int index){
+  void _deleteContact(int index) {
     setState(() {
       _contacts.removeAt(index);
     });
   }
 
-  void _showDeleteDialog(int index){
-
+  void _showDeleteDialog(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Delete Contact"),
+          content: const Text("Are you sure you want to delete this contact?"),
+          actions: [
+            IconButton(
+              onPressed: () => Navigator.pop(context),
+              icon: const Icon(Icons.cancel, color: Colors.grey),
+            ),
+            IconButton(
+              onPressed: () {
+                _deleteContact(index);
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.delete, color: Colors.red),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -78,7 +101,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   TextFormField(
                     controller: nameController,
                     decoration: const InputDecoration(
-                        border: OutlineInputBorder(), hintText: "Name"),
+                      border: OutlineInputBorder(),
+                      hintText: "Name",
+                    ),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return "Please Enter Your Name";
@@ -116,9 +141,10 @@ class _MyHomePageState extends State<MyHomePage> {
                       child: const Text(
                         "Add",
                         style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
@@ -131,24 +157,24 @@ class _MyHomePageState extends State<MyHomePage> {
               child: ListView.builder(
                 itemCount: _contacts.length,
                 itemBuilder: (context, index) {
-                  return ElevatedButton(
-                    onPressed: _deleteContact(index),
-                        child: Card(
+                  return GestureDetector(
+                    onLongPress: () => _showDeleteDialog(index),
+                    child: Card(
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      // margin: const EdgeInsets.symmetric(vertical: 5),
                       child: ListTile(
                         title: Text(_contacts[index]["name"]!),
-                        trailing: Icon(Icons.call),
                         subtitle: Text(_contacts[index]["number"]!),
-                        leading: Icon(Icons.person),
-                        // onLongPress: () => _showDeleteDialog(index),
+                        leading: const Icon(Icons.person,size: 40,),
+                        trailing: const Icon(Icons.call,size: 30,color: Colors.blueAccent,),
+                        titleTextStyle: TextStyle(color: Colors.red.shade500,fontWeight: FontWeight.bold,fontSize: 15),
                       ),
                     ),
                   );
                 },
               ),
             ),
-
-
-
           ],
         ),
       ),
